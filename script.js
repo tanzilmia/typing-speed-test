11a6c376f035989f36b29ee976796da440ea4ebe
tanzilmia <ziltan590@gmail.com>
2022-09-14 22:18:11 +0600

    decimal second control on history and save box

--- script.js
+++ script.js
@@ -67,12 +67,13 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
   resultModal.classList.toggle("hidden");
   modalBackground.classList.toggle("hidden");
+  
   // clear user text
   display.innerHTML = "";
   // make it inactive
